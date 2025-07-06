4b73c535fd109ba5b2955c5c7a03ebfdfdff71b0
umair379 <160377759+umair379@users.noreply.github.com>
2025-07-06 17:56:43 +0500

    Update main.py

--- comments/main.py
+++ comments/main.py
@@ -2,7 +2,7 @@
 print("Nimra")
 
 
-# The sentence written inside """ """ will not appear in the output; we can also write code inside it.
+# The sentence written inside """ """ will not appear in the output. We can also write code inside it. This is called a docstring.
 """
 This program will take the user's name
 and greet them
