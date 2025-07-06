d1780fce0d92b1fe6b943e34c74fdf51836e56fc
umair379 <160377759+umair379@users.noreply.github.com>
2025-07-06 17:54:41 +0500

    Update main.py

--- comments/main.py
+++ comments/main.py
@@ -2,6 +2,7 @@
 print("Nimra")
 
 
+# The sentence written inside """ """ will not appear in the output; we can also write code inside it.
 """
 This program will take the user's name
 and greet them
