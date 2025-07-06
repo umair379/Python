0bebe941d272436d0222336cb0318eacbef4c903
umair379 <160377759+umair379@users.noreply.github.com>
2025-07-06 18:41:25 +0500

    arithmetic operator

---
+++ arithmetic_operator/main.py
@@ -0,0 +1,29 @@
+# Basic Arithmetic Operations
+a = 10
+b = 3
+print(a + b)   # Addition → 10 + 3 = 13
+print(a - b)   # Subtraction → 10 - 3 = 7
+print(a * b)   # Multiplication → 10 * 3 = 30
+print(a / b)   # Division → 10 / 3 = 3.3333
+
+# Special Operators
+x = 10
+y = 3
+
+print(x % y)   # Modulus → 10 % 3 = 1 (Remainder)
+print(x ** y)  # Exponentiation → 10^3 = 1000 (Power)
+print(x // y)  # Floor Division → 10 // 3 = 3 (Only the integer part)
+
+# Using Arithmetic Operators with Variables
+num1 = 15
+num2 = 4
+
+sum_result = num1 + num2  # Addition
+sub_result = num1 - num2  # Subtraction
+mul_result = num1 * num2  # Multiplication
+div_result = num1 / num2  # Division
+
+print("Addition:", sum_result)
+print("Subtraction:", sub_result)
+print("Multiplication:", mul_result)
+print("Division:", div_result)
